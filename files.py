d5a21ce51a5402b849206919f9919e081d992280
sadxxzee <luiz.reis97ana@gmail.com>
2026-06-09 20:05:43 -0300

    calculo via terminal

--- files.py
+++ files.py
@@ -1,1 +1,12 @@
-teste
+total = float(input('insira o valor da sua conta: '))
+
+gorjeta= float (input("insira a porcentagem da gorjeta: "))
+
+novoTotal= total * gorjeta /100 + total
+
+clientes= int(input("insira enquantas pessoas vc ira dividir: "))
+
+valorIndividual= novoTotal / clientes
+
+print("R$" + str(novoTotal))
+print("R$" + str(valorIndividual))
